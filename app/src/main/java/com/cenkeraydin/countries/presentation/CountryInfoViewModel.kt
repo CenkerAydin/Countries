0a95c104a6851fe177bae62eeda4f0aa3088d1f6
CenkerAydin <20210808002@ogr.akdeniz.edu.tr>
2025-01-16 15:16:40 +0300

    add baseviewmodel and change the viewmodels

--- app/src/main/java/com/cenkeraydin/countries/presentation/CountryInfoViewModel.kt
+++ app/src/main/java/com/cenkeraydin/countries/presentation/CountryInfoViewModel.kt
@@ -1,14 +1,21 @@
 package com.cenkeraydin.countries.presentation
 
+import android.app.Application
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.cenkeraydin.countries.data.model.Country
+import com.cenkeraydin.countries.service.CountryDB
+import kotlinx.coroutines.launch
+import java.util.UUID
 
-class CountryInfoViewModel:ViewModel() {
-    val country = MutableLiveData<Country>()
+class CountryInfoViewModel(application: Application):BaseViewModel(application) {
+    val countryLiveData = MutableLiveData<Country>()
 
-    fun getDataFromRoom(){
-        val country = Country("Turkey","Asia","Ankara","TRY","Turkish","www.ss.com")
-        this.country.value = country
+    fun getDataFromRoom(uuid: Int){
+        launch {
+            val dao = CountryDB(getApplication()).countryDao()
+            val country = dao.getCountry(uuid)
+            countryLiveData.value = country
+        }
     }
 }
--- app/src/main/java/com/cenkeraydin/countries/presentation/CountryViewModel.kt
+++ app/src/main/java/com/cenkeraydin/countries/presentation/CountryViewModel.kt
@@ -1,23 +1,40 @@
 package com.cenkeraydin.countries.presentation
 
+import android.app.Application
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
 import com.cenkeraydin.countries.data.model.Country
 import com.cenkeraydin.countries.service.CountryAPIService
+import com.cenkeraydin.countries.service.CountryDB
+import com.cenkeraydin.countries.util.CustomSharedPreferences
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.observers.DisposableSingleObserver
 import io.reactivex.schedulers.Schedulers
+import kotlinx.coroutines.launch
 
-class CountryViewModel:ViewModel() {
+class CountryViewModel(application: Application) : BaseViewModel(application) {
     private val countryAPIService = CountryAPIService()
-    private val disposable =CompositeDisposable()
+    private val disposable = CompositeDisposable()
+    private var customPreferences = CustomSharedPreferences(getApplication())
     val countries = MutableLiveData<List<Country>>()
     val countryError = MutableLiveData<Boolean>()
     val countryLoading = MutableLiveData<Boolean>()
+    private var refresthTime = 10 * 60 * 1000 * 1000 * 1000L
 
     fun refreshData() {
-        getDataFromAPI()
+        val updateTime = customPreferences.getTime()
+        if (updateTime != null && updateTime != 0L && System.nanoTime() - updateTime < refresthTime) {
+            getDataFromSQLite()
+        } else {
+            getDataFromAPI()
+        }
+    }
+
+    private fun getDataFromSQLite() {
+        launch {
+            val countries = CountryDB(getApplication()).countryDao().getAllCountries()
+            showCountries(countries)
+        }
     }
 
     private fun getDataFromAPI() {
@@ -29,9 +46,7 @@ class CountryViewModel:ViewModel() {
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribeWith(object : DisposableSingleObserver<List<Country>>() {
                     override fun onSuccess(t: List<Country>) {
-                        countries.value = t
-                        countryError.value = false
-                        countryLoading.value = false
+                        storeInSQLite(t)
                     }
 
                     override fun onError(e: Throwable) {
@@ -45,4 +60,29 @@ class CountryViewModel:ViewModel() {
         )
     }
 
+    private fun showCountries(countryList: List<Country>) {
+        countries.value = countryList
+        countryError.value = false
+        countryLoading.value = false
+    }
+
+    private fun storeInSQLite(list: List<Country>) {
+        launch {
+            val dao = CountryDB(getApplication()).countryDao()
+            dao.deleteAllCountries()
+            val listLong = dao.insertAll(*list.toTypedArray())
+            var i = 0
+            while (i < list.size) {
+                list[i].uuid = listLong[i].toInt()
+                i++
+            }
+            showCountries(list)
+        }
+        customPreferences.saveTime(System.nanoTime())
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        disposable.clear()
+    }
 }
